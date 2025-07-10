42f002f933a5f7c004963d9c2cd7b377d8b87e75
Miner3DGaming <105589865+Miner3DGaming@users.noreply.github.com>
2025-07-11 00:24:00 +0200

    Added docstrings, more aliases, more unicodes, updated description, cleaned up some stuff. Increased compression to ~5%

--- src/internal.rs
+++ src/internal.rs
@@ -1,19 +1,31 @@
 use std::collections::HashMap;
 
-pub fn convert_to_replaceable(txt: &str) -> HashMap<String, String> {
+/// Takes in a raw file (in string form) and converts it to a hashmap while ignoring comments
+/// file_name is only used for debugging purposes
+pub fn convert_to_replaceable(
+    file_content: &str,
+    file_name: String,
+) -> HashMap<String, String> {
     let mut map = HashMap::new();
     let mut idx = 0;
-    for line in txt.lines() {
+    for line in file_content.lines() {
         idx += 1;
         if line.is_empty() || line.starts_with('#') {
             continue;
         }
+        let line = line.trim();
         if line.chars().filter(|c| *c == ' ').count() != 1 {
-            panic!("Invalid amount of spaces at line {}", idx);
+            panic!("Invalid amount of spaces at line {} of {}", idx, file_name);
         }
         let (original, replacement) = line.split_once(' ').unwrap();
         if map.contains_key(original) {
-            panic!("Duplicate key {} at line {}", original, idx);
+            panic!(
+                "Duplicate key {} at line {} of {}",
+                original, idx, file_name
+            );
+        }
+        if replacement.chars().count() != 1 {
+            println!("WARNING: Replacement on line {} does have a single character as replacement of {}", idx, file_name);
         }
         map.insert(original.to_string(), replacement.to_string());
     }
@@ -22,19 +34,23 @@ pub fn convert_to_replaceable(txt: &str) -> HashMap<String, String> {
 
 pub fn get_common() -> HashMap<String, String> {
     let common_txt = include_str!("common.txt");
-    return convert_to_replaceable(common_txt);
+    return convert_to_replaceable(common_txt, "Common".into());
 }
 pub fn get_uncommon() -> HashMap<String, String> {
     let uncommon_txt = include_str!("uncommon.txt");
-    return convert_to_replaceable(uncommon_txt);
+    return convert_to_replaceable(uncommon_txt, "Uncommon".into());
 }
 pub fn get_alias() -> Vec<Vec<String>> {
     let alias_txt = include_str!("alias.txt");
     let mut alias = Vec::new();
     for line in alias_txt.lines() {
-        if line.is_empty() || line.starts_with('#') || line.contains(" ") {
+        if line.is_empty() || line.starts_with('#') {
             continue;
         }
+        // if line.contains(" "){
+        //     panic!("Non comment line in alias.txt contains space")
+        // }
+        let line = line.replace(" ", "");
         let mut aliased = Vec::new();
         for c in line.chars() {
             aliased.push(c.into());
@@ -49,9 +65,14 @@ pub fn optimize_without_watching_case(
 ) -> String {
     let mut s = string.to_string();
     for (original, replacement) in map.iter() {
-        let temp = s.to_lowercase().replace(original, replacement);
-        if temp != s.to_lowercase() {
-            s = temp;
+        // Check if the string has a replacement in the current casing before replacing without caring
+        let before = s.clone();
+        s = s.replace(original, replacement);
+        if s == before {
+            let temp = s.to_lowercase().replace(original, replacement);
+            if temp != s.to_lowercase() {
+                s = temp;
+            }
         }
     }
     return s;
@@ -60,9 +81,9 @@ pub fn optimize_without_watching_case(
 pub fn optimize_loosely_without_watching_case(
     string: &str,
     map: &HashMap<String, String>,
+    aliases: Vec<Vec<String>>,
 ) -> String {
     let mut s = string.to_string();
-    let aliases = get_alias();
     let mut found;
     for (original, replacement) in map.iter() {
         found = false;
@@ -84,17 +105,25 @@ pub fn optimize_loosely_without_watching_case(
         if found {
             continue;
         }
-        let temp = s.replace(original, replacement);
-        if temp != s.to_lowercase() {
-            s = temp;
+        let before = s.clone();
+        s = s.replace(original, replacement);
+        if s == before {
+            let temp = s.replace(original, replacement);
+            if temp != s.to_lowercase() {
+                s = temp;
+            }
         }
     }
     return s;
 }
 
-pub fn optimize_loosely(string: &str, map: &HashMap<String, String>) -> String {
+/// Replace every instance of x with y of {x: y} while
+pub fn optimize_loosely(
+    string: &str,
+    map: &HashMap<String, String>,
+    aliases: Vec<Vec<String>>,
+) -> String {
     let mut s = string.to_string();
-    let aliases = get_alias();
     let mut found;
     for (original, replacement) in map.iter() {
         found = false;
@@ -120,7 +149,7 @@ pub fn optimize_loosely(string: &str, map: &HashMap<String, String>) -> String {
     }
     return s;
 }
-
+/// Replace every instance of x with y of {x: y}
 pub fn optimize(string: &str, map: &HashMap<String, String>) -> String {
     let mut s = string.to_string();
     for (original, replacement) in map.iter() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod internal;
 
+/// Replace the text using more common character sequences
 pub fn replace_common(
     string: &str,
     watch_casing: bool,
@@ -10,6 +11,7 @@ pub fn replace_common(
             return internal::optimize_loosely_without_watching_case(
                 string,
                 &internal::get_common(),
+                internal::get_alias(),
             );
         } else {
             return internal::optimize_without_watching_case(
@@ -19,12 +21,17 @@ pub fn replace_common(
         }
     } else {
         if loosely {
-            return internal::optimize_loosely(string, &internal::get_common());
+            return internal::optimize_loosely(
+                string,
+                &internal::get_common(),
+                internal::get_alias(),
+            );
         } else {
             return internal::optimize(string, &internal::get_common());
         }
     }
 }
+/// Replace the text using more uncommon character sequences
 pub fn replace_uncommon(
     string: &str,
     watch_casing: bool,
@@ -35,6 +42,7 @@ pub fn replace_uncommon(
             return internal::optimize_loosely_without_watching_case(
                 string,
                 &internal::get_uncommon(),
+                internal::get_alias(),
             );
         } else {
             return internal::optimize_without_watching_case(
@@ -47,6 +55,7 @@ pub fn replace_uncommon(
             return internal::optimize_loosely(
                 string,
                 &internal::get_uncommon(),
+                internal::get_alias(),
             );
         } else {
             return internal::optimize(string, &internal::get_uncommon());
@@ -54,10 +63,36 @@ pub fn replace_uncommon(
     }
 }
 
+/// Replace the text using common and uncommon character sequences
 pub fn replace_all(string: &str, watch_casing: bool, loosely: bool) -> String {
     return replace_common(
         &replace_uncommon(string, watch_casing, loosely),
         watch_casing,
         loosely,
     );
+}
+/// Replace the text with a custom hashmap easily created with [crate::internal::convert_to_replaceable]
+/// The alias vec contains equivilants -> * can also be used as °, to get the default aliases use [crate::internal::get_alias]
+pub fn replace_custom(
+    string: &str,
+    watch_casing: bool,
+    loosely: bool,
+    map: &std::collections::HashMap<String, String>,
+    aliases: Vec<Vec<String>>,
+) -> String {
+    if watch_casing {
+        if loosely {
+            return internal::optimize_loosely_without_watching_case(
+                string, map, aliases,
+            );
+        } else {
+            return internal::optimize_without_watching_case(string, map);
+        }
+    } else {
+        if loosely {
+            return internal::optimize_loosely(string, map, aliases);
+        } else {
+            return internal::optimize(string, map);
+        }
+    }
 }
